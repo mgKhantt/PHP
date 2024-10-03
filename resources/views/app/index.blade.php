5af4d077f00c4b7414e80f57a1eb1c8e25906a73
mgKhantt <khantphonenaing156@gamil.com>
2024-10-03 20:42:40 +0630

    Fixed

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -55,7 +55,7 @@
                 <input type="text" class="form-control" placeholder="Subscribe Email">
                 <button class="input-group-text">Subscribe</button>
             </div>
-        </form>        
+        </form>
     </div>
 
     <!-- about -->
@@ -73,14 +73,14 @@
         </div>
     </div>
     <div id="about" class="container-fluid bg-gray">
-        <div class="row">          
+        <div class="row">
             <div class="col-md-4 d-flex justify-content-center align-items-center">
                 <i class="fa-solid fa-globe icon-lg slideObject"></i>
             </div>
             <div class="col-md-8">
                 <h2>OUR VALUES</h2>
                 <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>                
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
             </div>
         </div>
     </div>
@@ -94,44 +94,51 @@
                 <i class="fa-solid fa-power-off icon-sm"></i>
                 <h4>POWER</h4>
                 <p>Lorem ipsum dolor sit amet..</p>
-            </div>  
+            </div>
             <div class="col-md-4">
                 <i class="fa-solid fa-heart icon-sm"></i>
                 <h4>LOVE</h4>
                 <p>Lorem ipsum dolor sit amet..</p>
-            </div>  
+            </div>
             <div class="col-md-4">
                 <i class="fa-solid fa-lock icon-sm"></i>
                 <h4>JOB DONE</h4>
                 <p>Lorem ipsum dolor sit amet..</p>
-            </div>            
+            </div>
         </div>
         <div class="row slideObject">
             <div class="col-md-4">
                 <i class="fa-solid fa-leaf icon-sm"></i>
                 <h4>GREEN</h4>
                 <p>Lorem ipsum dolor sit amet..</p>
-            </div>  
+            </div>
             <div class="col-md-4">
                 <i class="fa-solid fa-certificate icon-sm"></i>
                 <h4>CERTIFIED</h4>
                 <p>Lorem ipsum dolor sit amet..</p>
-            </div>  
+            </div>
             <div class="col-md-4">
                 <i class="fa-solid fa-wrench icon-sm"></i>
                 <h4>HARD WORK</h4>
                 <p>Lorem ipsum dolor sit amet..</p>
-            </div>            
+            </div>
         </div>
-   
+
     </div>
 
     <!-- portfolio -->
     <div id="portfolio" class="text-center container-fluid bg-gray">
         <h2>Portfolio</h2>
         <h4>What we have created</h4>
-        <div class="row slideObject">
+        <div class="row slideObject d-flex align-items-center justify-content-center">
+            @foreach ($portfolios as $portfolio)
             <div class="col-md-4">
+                <img src="{{ asset('/storage/' . $portfolio->image) }}" class="mb-3 img-thumbnail">
+                <p>{{$portfolio->title}}</p>
+                <p>{{$portfolio->description}}.</p>
+            </div>
+            @endforeach
+            <!-- <div class="col-md-4">
                 <img src="{{ asset('/storage/app/images/paris.jpg') }}" alt="paris" class="mb-3 img-thumbnail">
                 <p>Paris</p>
                 <p>We created Paris.</p>
@@ -145,7 +152,7 @@
                 <img src="{{ asset('/storage/app/images/sanfran.jpg') }}" alt="sanfran" class="mb-3 img-thumbnail">
                 <p>Sanfrancisco</p>
                 <p>We created Sanfrancisco.</p>
-            </div>
+            </div> -->
         </div>
 
         <h2>What our customer say</h2>
@@ -153,9 +160,9 @@
             <div class="col-md-12">
                 <div id="myCarousel" class="carousel slide">
                     <div class="carousel-indicators">
-                      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
                     </div>
                     <div class="carousel-inner">
                         <div class="carousel-item active">
@@ -174,9 +181,9 @@
                     </button>
                     <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
                         <i class="fa-solid fa-chevron-right"></i>
-                      <span class="visually-hidden">Next</span>
+                        <span class="visually-hidden">Next</span>
                     </button>
-                  </div>
+                </div>
             </div>
         </div>
     </div>
@@ -187,23 +194,23 @@
         <div class="row slideObject">
             <div class="col-md-4">
                 <div class="panel">
-                        <div class="panel-header">
-                            <h1>Basic</h1>
-                        </div>
-                        <div class="panel-body">
-                            <p>20 Lorem</p>
-                            <p>15 Lorem</p>
-                            <p>10 Lorem</p>
-                            <p>5 Lorem</p>
-                            <p>Endless Amet</p>
-                        </div>
-                        <div class="panel-footer">
-                            <h3>$30</h3>
-                            <h4>Per Month</h4>
-                            <button class="btn btn-lg btn-orange mb-3">Sign Up</button>
-                        </div>
+                    <div class="panel-header">
+                        <h1>Basic</h1>
+                    </div>
+                    <div class="panel-body">
+                        <p>20 Lorem</p>
+                        <p>15 Lorem</p>
+                        <p>10 Lorem</p>
+                        <p>5 Lorem</p>
+                        <p>Endless Amet</p>
+                    </div>
+                    <div class="panel-footer">
+                        <h3>$30</h3>
+                        <h4>Per Month</h4>
+                        <button class="btn btn-lg btn-orange mb-3">Sign Up</button>
                     </div>
                 </div>
+            </div>
             <div class="col-md-4">
                 <div class="panel">
                     <div class="panel-header">
@@ -247,41 +254,41 @@
 
     <!-- contact -->
     <div id="contact" class="container-fluid bg-gray">
-    <h2 class="text-center">Contact</h2>
-    <div class="row">
-        <div class="col-md-5">
-            <p>Contact us and we'll get back to you within 24 hours.</p>
-            <p><i class="fas fa-map-marker-alt"></i>&nbsp;&nbsp;Chicago, US</p>
-            <p><i class="fas fa-phone"></i>&nbsp;&nbsp;+00 1515151515</p>
-            <p><i class="fas fa-envelope"></i>&nbsp;&nbsp;mymail@gmail.com</p>
-        </div>
-        <div class="col-md-7 slideObject">
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="mb-3">
-                        <input type="text" class="form-control" placeholder="name">
+        <h2 class="text-center">Contact</h2>
+        <div class="row">
+            <div class="col-md-5">
+                <p>Contact us and we'll get back to you within 24 hours.</p>
+                <p><i class="fas fa-map-marker-alt"></i>&nbsp;&nbsp;Chicago, US</p>
+                <p><i class="fas fa-phone"></i>&nbsp;&nbsp;+00 1515151515</p>
+                <p><i class="fas fa-envelope"></i>&nbsp;&nbsp;mymail@gmail.com</p>
+            </div>
+            <div class="col-md-7 slideObject">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="mb-3">
+                            <input type="text" class="form-control" placeholder="name">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="mb-3">
+                            <input type="email" class="form-control" placeholder="email">
+                        </div>
                     </div>
                 </div>
-                <div class="col-md-6">
-                    <div class="mb-3">
-                        <input type="email" class="form-control" placeholder="email">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="mb-3">
+                            <textarea class="form-control" style="height: 150px;"></textarea>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <button class="btn btn-orange">Send</button>
                     </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="mb-3">
-                        <textarea class="form-control" style="height: 150px;"></textarea>
-                    </div>                        
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12">
-                    <button class="btn btn-orange">Send</button>
-                </div>
-            </div>
         </div>
-    </div>
     </div>
 
     <!-- location map -->
@@ -304,32 +311,32 @@
     <script src="{{ asset('/storage/app/vendors/jquery/jquery-3.7.1.js') }}"></script>
     <script>
         $(document).ready(() => {
-            $('.nav-item').click((event)=>{
+            $('.nav-item').click((event) => {
                 $('.nav-item a').removeClass('active');
                 const element = $(event.currentTarget).children().eq(0);
                 element.addClass('active');
             });
 
-            $(window).scroll(()=>{                
+            $(window).scroll(() => {
                 // winTop > (objectTop - 200)   
                 const winTop = $(window).scrollTop(); //1900
                 // console.log(winTop);
 
-                const slideObjects = $('.slideObject');                
+                const slideObjects = $('.slideObject');
 
-                slideObjects.each( (index,slideObject) => {
+                slideObjects.each((index, slideObject) => {
                     const slideObjectTop = $(slideObject).offset().top; // 2400
                     // console.log(slideObject,slideObjectTop)
 
                     //debugging code
-                    console.log('object',index);
+                    console.log('object', index);
                     console.log("wintop", winTop);
                     console.log('slideObject Top - 600', slideObjectTop - 600);
                     console.log(winTop > slideObjectTop - 600)
                     console.log('------------------------');
                     //end
-                    
-                    if(winTop > slideObjectTop - 600){ // 1800
+
+                    if (winTop > slideObjectTop - 600) { // 1800
                         $(slideObject).addClass('slide');
                     }
 
